929a698cf99db4595f69a494b4ce915905e31712
Antony Kowalski <beheaded_96@mail.ru>
2025-07-24 18:43:44 +0300

    стили

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,18 +1,17 @@
 import Image from 'next/image';
 import { imageData } from './data';
-import styles from './Projects.module.css'
-
+import styles from './Projects.module.css';
 
 export default function Projects() {
 	return (
 		<section id='projects' className={styles.projects}>
 			<div className='container'>
-				<h2 className={styles.project__title}>Технологии</h2>
-				<div className='technologies__grid'>
+				<h2 className={styles.projects__title}>Технологии</h2>
+				<div className={styles.technologies__grid}>
 					{imageData.map((image, i) => (
-						<div key={i} className='technologies__item'>
-							<div className='technologies__image'>
-								<Image src={image} alt='image'/>
+						<div key={i} className={styles.technologies__item}>
+							<div className={styles.technologies__image}>
+								<Image src={image} alt='technology' width={80} height={80} />
 							</div>
 						</div>
 					))}
